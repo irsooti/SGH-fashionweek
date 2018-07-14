0e845ee0126481e8ca675536e261595fba28963b
Daniele <irsooti@gmail.com>
2018-07-14 21:47:03 +0200

    Updates

--- flickity.f8eb570a.js
+++ flickity.f8eb570a.js
@@ -164,11 +164,12 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"..\\node_modules\\parcel\\src\\builtins\\bundle-url.js"}],"assets\\scss\\main.scss":[function(require,module,exports) {
+},{"./bundle-url":"..\\node_modules\\parcel\\src\\builtins\\bundle-url.js"}],"..\\node_modules\\flickity\\css\\flickity.css":[function(require,module,exports) {
 
-var reloadCSS = require('_css_loader');
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
 },{"_css_loader":"..\\node_modules\\parcel\\src\\builtins\\css-loader.js"}],"..\\node_modules\\parcel\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
