a1d5b7b4deed5ae0fb70403e0bb46d7339ee0d7b
Daniele <irsooti@gmail.com>
2018-07-14 16:49:59 +0200

    mobile first (some fixes)

---
+++ scripts/copy.js
@@ -0,0 +1,25 @@
+var ncp = require('ncp').ncp;
+
+ncp.limit = 16;
+
+ncp('src/assets/fonts', 'dist/fonts', function (err) {
+  if (err) {
+    return console.error('👀 oops...', err);
+  }
+  console.log('✔ Font copied!');
+});
+
+ncp('src/assets/icons', 'dist/icons', function (err) {
+  if (err) {
+    return console.error('👀 oops...', err);
+  }
+  console.log('✔ Icons copied!');
+});
+
+ncp('scripts/gh', 'dist', function (err) {
+  if (err) {
+    return console.error('👀 oops...', err);
+  }
+  console.log('✔ CNAME copied!');
+})
+
